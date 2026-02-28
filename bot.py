a92ef4a8096eb9c1dee315bb327cfd0a912af261
AdityaVardhanaRentala <adityavardhana.r@gmail.com>
2026-02-28 18:02:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ import datetime
 import pytz
 from flask import Flask
 import threading
+print("=== VERSION CHECK 28 FEB 1 ===")
 
 # ---------------- WEB SERVER FOR RENDER ---------------- #
 
@@ -205,6 +206,7 @@ if __name__ == "__main__":
         exit(1)
 
     bot.run(TOKEN)
+
 
 
 
